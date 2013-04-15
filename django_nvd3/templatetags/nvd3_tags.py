23a17d478f7eddca2bbf6b4051850fb0a0044c53
Areski Belaid <areski@gmail.com>
2013-04-16 01:15:18 +0200

    update structure

---
+++ django_nvd3/templatetags/nvd3_tags.py
@@ -0,0 +1,42 @@
+from django import template
+from django.template.defaultfilters import register, \
+    stringfilter
+from django.utils.safestring import mark_safe
+#from django.conf import settings
+#from django.utils.translation import ugettext_lazy as _
+import datetime
+
+
+@register.assignment_tag(name='load_nvd3')
+def load_nvd3(chart_type, series, render_to=''):
+    """Loads the ``Chart`` objects in the ``chart_list`` to the
+    HTML elements with id's specified in ``render_to``.
+
+    **Arguments**:
+
+    - **render_to** - id where the chart needs to be rendered to.
+    """
+    js_script = (
+        '<script type="text/javascript">\n'
+        'var test=1;\n\n'
+        'alert(\'NVD3\');\n</script>')
+    return mark_safe(js_script)
+
+
+# @register.filter(name='cut')
+# def cut(value, arg):
+#     """Removes all values of arg from the given string"""
+#     return value.replace(arg, '')
+
+
+@register.filter
+@stringfilter
+def superlower(value):
+    return value.lower()
+
+# Template usage
+# {% get_current_time "%Y-%m-%d %I:%M %p" as the_time %}
+# <p>The time is {{ the_time }}.</p>
+@register.assignment_tag(name='get_current_time')
+def get_current_time(format_string):
+    return datetime.datetime.now().strftime(format_string)
---
+++ django_nvd3/tests.py
@@ -0,0 +1,10 @@
+# from common.utils import BaseAuthenticatedClient
+# from nvd3.views import somemethod
+
+
+# class NVD3View(BaseAuthenticatedClient):
+#     """Test Function to check ..."""
+
+#     def test_writetest(self):
+#         """Test Function to check"""
+#         self.assertEqual(1, 4)
