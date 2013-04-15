18e57df651ddc134d04f7f328434a08e44cae766
Areski Belaid <areski@gmail.com>
2013-04-16 00:59:41 +0200

    update

--- nvd3/urls.py
+++ nvd3/urls.py
@@ -1,2 +1,1 @@
-from django.conf.urls import patterns, include
-
+# from django.conf.urls import patterns, include
--- nvd3/views.py
+++ nvd3/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render_to_response
-from django.http import HttpResponseRedirect
-from django.utils.translation import ugettext_lazy as _
+# from django.shortcuts import render_to_response
+# from django.http import HttpResponseRedirect
+# from django.utils.translation import ugettext_lazy as _
 
 
-def user_count(user):
-    """Get count of logged in user's notifications"""
-    return 10
+# def user_count(user):
+#     """Get count of logged in user's notifications"""
+#     return 10
