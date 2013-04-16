a133ada7cfe352e3c93b140a544e53537682b7be
Shrenik Patel <shrenik181986@gmail.com>
2013-04-16 18:02:22 +0530

    update template

--- demoproject/demoproject/views.py
+++ demoproject/demoproject/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render_to_response
+from django.template.context import RequestContext
 
 
 def home(request):
@@ -8,5 +9,9 @@ def home(request):
     xdata = ["Orange", "Banana", "Pear", "Kiwi", "Apple", "Strawberry", "Pineapple"]
     ydata = [3, 4, 0, 1, 5, 7, 3]
     chartdata = {'x': xdata, 'y': ydata}
-
-    return render_to_response('home.html', chartdata)
+    data = {
+        'test': 1,
+        'chartdata': chartdata
+    }
+    return render_to_response('home.html', data,
+           context_instance=RequestContext(request))
