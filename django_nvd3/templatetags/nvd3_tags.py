ebe5a457aa3582618a1a612a8f438224d18461ce
Areski Belaid <areski@gmail.com>
2013-04-16 19:01:40 +0200

    fix pieChart

--- django_nvd3/templatetags/nvd3_tags.py
+++ django_nvd3/templatetags/nvd3_tags.py
@@ -1,76 +1,66 @@
-from django import template
-from django.template.defaultfilters import register, \
-    stringfilter
+from django.template.defaultfilters import register
 from django.utils.safestring import mark_safe
 #from django.conf import settings
 #from django.utils.translation import ugettext_lazy as _
-import datetime
-
-#Usage
-#-----
-#
-# <head>
-#     <!-- code to include the NVD3 and D3 libraries goes here -->
-#     <!-- load_nvd3 filter takes a comma-separated list of id's where -->
-#     <!-- the charts need to be rendered to                             -->
-#     {% load nvd3 %}
-#     {% load_chart "pieChart" chartdata "container" %}
-# </head>
-# <body>
-#     <div id='container'> Chart will be rendered here </div>
-# </body>
-
-
-# @register.simple_tag(name='get_app_name')
-# def get_app_name(app_label, model_name, object_id):
-#     """To get app name from app_label, model_name & object_id
-#     Usage: {% get_app_name app_label model_name object_id %}
-#     """
-#     try:
-#         return get_model(app_label, model_name).objects.get(pk=object_id)
-#     except:
-#         return '-'
+#import datetime
+from nvd3 import lineWithFocusChart, lineChart, \
+    multiBarChart, pieChart, stackedAreaChart, \
+    multiBarHorizontalChart, linePlusBarChart, \
+    cumulativeLineChart, discreteBarChart, scatterChart
+from nvd3.NVD3Chart import NVD3Chart
 
 
 @register.simple_tag(name='load_chart')
-def load_chart(chart_type, series, render_to=''):
-    """Loads the ``Chart`` objects in the ``chart_list`` to the
-    HTML elements with id's specified in ``render_to``.
+def load_chart(chart_type, series, container, height=400, width=400):
+    """Loads the Chart objects in the container.
+
 
     **Arguments**:
 
     - **render_to** - id where the chart needs to be rendered to.
     """
-    from nvd3 import pieChart
+    if chart_type == 'pieChart':
+        chart = eval(chart_type)(name=container, height=height, width=width)
+        xdata = series['x']
+        ydata = series['y']
 
-    chart = eval(chart_type)(name=chart_type, height=400, width=400)
-    chart.set_containerheader("\n\n<h2>" + chart_type + "</h2>\n\n")
-    xdata = series['x']
-    ydata = series['y']
+        chart.add_serie(y=ydata, x=xdata)
 
-    chart.add_serie(y=ydata, x=xdata)
     chart.buildhtml()
 
-    html_string = chart.htmlheader + '\n' + chart.jschart + '\n' + chart.container
+    html_string = chart.jschart + '\n'
     return mark_safe(html_string)
 
 
-# @register.filter(name='cut')
-# def cut(value, arg):
-#     """Removes all values of arg from the given string"""
-#     return value.replace(arg, '')
+@register.simple_tag(name='include_nvd3jscss')
+def include_nvd3jscss():
+    """
+    Include the javascript and css for nvd3
+    This include :
+        * d3.v2.js
+        * nv.d3.js
+        * nv.d3.css
+    """
+    chart = NVD3Chart()
+    chart.buildhtmlheader()
+    return mark_safe(chart.htmlheader + '\n')
 
 
-@register.filter
-@stringfilter
-def superlower(value):
-    return value.lower()
+@register.simple_tag(name='include_container')
+def include_container(include_container, height=400, width=600):
+    """
+    Include the html for the chart container and css for nvd3
+    This will include something similar as :
+        <div id="containername"><svg style="height:400px;width:600px;"></svg></div>
+    """
+    chart = NVD3Chart()
+    chart.buildhtmlheader()
+    return mark_safe(chart.htmlheader + '\n')
 
 
 # Template usage
 # {% get_current_time "%Y-%m-%d %I:%M %p" as the_time %}
 # <p>The time is {{ the_time }}.</p>
-@register.assignment_tag(name='get_current_time')
-def get_current_time(format_string):
-    return datetime.datetime.now().strftime(format_string)
-
+# @register.assignment_tag(name='get_current_time')
+# def get_current_time(format_string):
+#     return datetime.datetime.now().strftime(format_string)
