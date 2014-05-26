3b36c9f91e9e0a916bb9a1df669f1ac9ecf89859
areski <areski@gmail.com>
2014-05-27 00:36:58 +0200

    add update version script

---
+++ update_version.sh
@@ -0,0 +1,10 @@
+git flow release start v$1
+sed -i -e "s/__version__ = '.*'/__version__ = '$1'/g" django_nvd3/__init__.py
+sed -i -e "s/version = '.*'/version = '$1'/g" docs/source/conf.py
+#rm -rf docs/html
+#python setup.py develop
+#make docs
+#git commit docs nvd3/__init__.py -m "Update to version v$1"
+git commit nvd3/__init__.py -m "Update to version v$1"
+git flow release finish v$1
+python setup.py sdist upload -r pypi
