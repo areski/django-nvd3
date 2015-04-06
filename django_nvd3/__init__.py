8e77f5fb0f200323164a839f61e670aac53468ea
areski <areski@gmail.com>
2015-04-06 16:18:58 +0200

    Update to version v0.8.1

--- django_nvd3/__init__.py
+++ django_nvd3/__init__.py
@@ -9,7 +9,7 @@ for d3.js without taking away the power that d3.js gives you.
 Project location : https://github.com/areski/django-nvd3
 """
 
-__version__ = '0.8.0'  # edit also docs/source/conf.py and update requirements.txt
+__version__ = '0.8.1'  # edit also docs/source/conf.py and update requirements.txt
 __author__ = "Areski Belaid"
 __contact__ = "areski@gmail.com"
 __homepage__ = "http://areskibelaid.com/"
