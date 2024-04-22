654b6df994dd9c7f687715c92a5df8749514b5d2
areski <areski@gmail.com>
2024-04-22 12:52:48 +0200

    [fix](version) release prep

--- update_version.sh
+++ update_version.sh
@@ -3,7 +3,7 @@
 #   ./update_version.sh 0.7.0
 #
 
-git flow release start v$1
+git flow release start $1
 sed -i -e "s/__version__ = '.*'/__version__ = '$1'/g" django_nvd3/__init__.py
 sed -i -e "s/version = '.*'/version = '$1'/g" docs/source/conf.py
 #rm -rf docs/html
@@ -11,7 +11,7 @@ sed -i -e "s/version = '.*'/version = '$1'/g" docs/source/conf.py
 #make docs
 #git commit docs django_nvd3/__init__.py -m "Update to version v$1"
 git commit -a -m "Update to version v$1"
-git flow release finish v$1
+git flow release finish $1
 python setup.py sdist
 twine upload dist/django-nvd3-$1.tar.gz
 git push origin develop; git push origin master; git push --tags
